7c73587b7e88c5bd881d79e8569501491f9fd4c3
Eugenia Zarza <eugenia.zarza@gmail.com>
2018-04-25 15:40:01 -0500

    set xhmm blocks in the right order

---
+++ 004-get-cnvs/config.mk
@@ -0,0 +1,9 @@
+#Path to xhmm executable
+XHMM=/media/genomics/disco1/Programs/xhmm/statgen-xhmm/xhmm
+
+#PATH to reference genome
+REFERENCE=/media/genomics/disco2/Databases/genome_references/hg19/hg19.fa
+
+#PATH to parameters file
+PARAMETERS=/home/ezarza/Projects/mk-xhmm/params.txt
+
