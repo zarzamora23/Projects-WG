7d40617432ecae8968c41c74ff89184c0db976df
Eugenia Zarza <eugenia.zarza@gmail.com>
2018-04-26 11:36:44 -0500

    Setting filter and parameter values with config files

--- 002mk-merge-coverage-center/config.mk
+++ 002mk-merge-coverage-center/config.mk
@@ -1,2 +1,17 @@
 #Path to xhmm executable
 XHMM=/media/genomics/disco1/Programs/xhmm/statgen-xhmm/xhmm
+
+#Values for variables applying filters
+#RD = read depth; MIN = minimum, MAX = maximum
+
+MIN_TARGET_SIZE=10
+MAX_TARGET_SIZE=10000
+MIN_MEAN_TARGET_RD=10
+MAX_MEAN_TARGET_RD=1500
+MIN_MEAN_SAMPLE_RD=25
+MAX_MEAN_SAMPLE_RD=600
+MAX_SD_SAMPLE_RD=400
+
+
+
+
--- 004-get-cnvs/config.mk
+++ 004-get-cnvs/config.mk
@@ -4,6 +4,32 @@ XHMM=/media/genomics/disco1/Programs/xhmm/statgen-xhmm/xhmm
 #PATH to reference genome
 REFERENCE=/media/genomics/disco2/Databases/genome_references/hg19/hg19.fa
 
-#PATH to parameters file
-PARAMETERS=/home/ezarza/Projects/mk-xhmm/params.txt
+#Value for variable 'QSOME' or 
+#-t --discoverSomeQualThresh=DOUBLE
+#                                Quality threshold for discovering a CNV in a 
+#                                  sample  (default=`30')
+
+QTHRESHOLD=30
+
+#PARAMETERS Values for XHMM that will be added to the parameter file
+# Here we show the default values
+#1. Exome-wide CNV rate						1e-08
+#2. Mean number of targets in CNV				6
+#3. Mean distance between targets within CNV (in KB)		70
+#4. Mean of DELETION z-score distribution			-3
+#5. Standard deviation of DELETION z-score distribution		1
+#6. Mean of DIPLOID z-score distribution			0
+#7. Standard deviation of DIPLOID z-score distribution		1
+#8. Mean of DUPLICATION z-score distribution			3
+#9. Standard deviation of DUPLICATION z-score distribution	1
+
+EXOME_CNV_RATE=1e-04
+MEAN_CNV_TARGETS=2
+MEAN_TARGET_DIsTANCE_KBs=35
+MEAN_DEL_ZSCORE_DIST=-3
+SD_DEL_ZSCORE_DIST=1
+MEAN_DIP_ZSCORE_DIST=0
+SD_DIP_ZSCORE_DIST=1
+MEAN_DUP_ZSCORE_DIST=3
+SD_DUP_ZSCORE_DIST=1
 
